c1bc8a5c5756353b98836ca28ab34c19144b6bc2
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2019-12-13 21:47:06 +0100

    Add text and scrollbars

--- application.py
+++ application.py
@@ -15,6 +15,11 @@ class Application:
         self.win.geometry("800x800")
         self.win.title("Text Analyzer")
         self.win.iconbitmap(default="icons/text_analyzer.ico")
+        self.scrollbar_text = tk.Scrollbar(self.win)
+        self.scrollbar_text.place(in_=self.text, relx=1., rely=0, relheight=1.)
+        self.scrollbar_text.config(command=self.text.yview)
+        self.text.config(yscrollcommand=self.scrollbar_text.set)
+        self.text.place(x=0, y=0, relwidth=1, relheight=1, width=- 18)
 
 
 app = Application()
