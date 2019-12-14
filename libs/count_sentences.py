47d583d928b329891107acc86fe792ad1251ba90
FraQu <FraQu@users.noreply.github.com>
2019-12-14 13:12:51 +0100

    Count... (#12)
    * Fix #2
    Add download file from web
    Add url
    
    * Fix #4
    Add count words
    
    * Fix #5
    Implement count punctuation marks
    
    * Fix  #6
    Implement count sentences
    
    * Fix #6
    FIX counting sentences

---
+++ libs/count_sentences.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import *
+from libs import file_name
+from tkinter import messagebox as msg
+
+
+def count_sentences():
+    ##
+    # Count sentences in file
+    ##
+    try:
+        file = open(file_name.name_file)
+        win = tk.Toplevel()
+        win.geometry("400x200")
+        win.title("Number of sentences")
+        data = file.read()
+        sentences = len(data.split("."))
+        text = Label(win, text="Number of sentences in text: ")
+        text.pack()
+        number_of_sentences = Label(win, text=sentences - 1)
+        number_of_sentences.pack()
+    except IOError:
+        msg.showinfo("There is no file!", "Download file...")
+
