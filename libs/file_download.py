81cb3bddb269600b1e69a2ed2041d6075af546fc
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2020-01-03 16:26:36 +0100

    Implement dialog window do download file: Yes or No.

--- libs/file_download.py
+++ libs/file_download.py
@@ -1,18 +1,23 @@
 import requests
 from libs import file_name, file_url
+from tkinter import messagebox
 
 
 def file_download():
     ###
     # Download file from url and save as 'name from fine_name.py' for later purposes.
     ###
-    url = file_url.url
-    filename = file_name.name_file
-    download_file = requests.get(url, stream=True)
+    answer = messagebox.askquestion("Download file?", "Do you want to download file?")
+    if answer == 'yes':
+        messagebox.showinfo('File downloaded...', 'File has been downloaded and saved to Application path.')
+        url = file_url.url
+        filename = file_name.name_file
+        download_file = requests.get(url, stream=True)
 
-    with open(filename, 'wb') as file:
-        for chunk in download_file.iter_content(chunk_size=1024):
-            if not chunk:
-                break
-            file.write(chunk)
-
+        with open(filename, 'wb') as file:
+            for chunk in download_file.iter_content(chunk_size=1024):
+                if not chunk:
+                    break
+                file.write(chunk)
+    else:
+        messagebox.showinfo('Download or open...', 'You need to download or open file first!')
