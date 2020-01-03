a6e2fe212557b1caa9e04439a1a59a839ebfa1b0
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2020-01-03 23:37:49 +0100

    Fix counting punctuation marks

--- libs/count_punctuation_marks.py
+++ libs/count_punctuation_marks.py
@@ -1,7 +1,9 @@
+import string
 import tkinter as tk
 from tkinter import *
-from libs import file_name
 from tkinter import messagebox as msg
+
+from libs import file_name
 
 
 def count_punctuation_marks():
@@ -14,15 +16,16 @@ def count_punctuation_marks():
         win.geometry("400x200")
         win.title("Number of punctuation marks")
         data = file.read()
-        count = 0
-        for i in range(0, len(data)):
-            if data[i] in (".", "?"):
-                count = count + 1
 
-        punctuation_marks = count
+        punctuation_marks = 0
+        for char in data:
+            if char in string.punctuation:
+                punctuation_marks = punctuation_marks + 1
+
         text = Label(win, text="Number of punctuation marks in text: ")
         text.pack()
         number_of_punctuation_marks = Label(win, text=punctuation_marks)
         number_of_punctuation_marks.pack()
     except IOError:
         msg.showinfo("There is no file!", "Download file...")
+
