782db55b60e49637f3550aa8e93e64a0ffe0a929
FraQu <FraQu@users.noreply.github.com>
2020-01-11 00:29:35 +0100

    Remove the nice code :X (#26)

--- libs/generate_raport.py
+++ libs/generate_raport.py
@@ -55,42 +55,18 @@ def generate():
         file = open(file_name.name_file, encoding="utf8")
         data = file.read()
 
-        counter = Counter(data)
-        consonants = counter['b'] + counter['B'] + counter['c'] + counter['C'] + counter['d'] + counter['D'] \
-                     + counter['f'] + counter['F'] + counter['g'] + counter['G'] + counter['h'] + counter['H'] \
-                     + counter['j'] + counter['J'] + counter['k'] + counter['K'] + counter['l'] + counter['L'] \
-                     + counter['m'] + counter['M'] + counter['n'] + counter['N'] + counter['p'] + counter['P'] \
-                     + counter['q'] + counter['Q'] + counter['r'] + counter['R'] + counter['s'] + counter['S'] \
-                     + counter['t'] + counter['T'] + counter['v'] + counter['V'] + counter['w'] + counter['W'] \
-                     + counter['x'] + counter['X'] + counter['z'] + counter['Z']
-        vowels = counter['a'] + counter['A'] + counter['e'] + counter['E'] + counter['i'] + counter['I'] \
-                 + counter['o'] + counter['O'] + counter['u'] + counter['U'] + counter['y'] + counter['Y']
-        a = counter['a'] + counter['A']
-        b = counter['b'] + counter['B']
-        c = counter['c'] + counter['C']
-        d = counter['d'] + counter['D']
-        e = counter['e'] + counter['E']
-        f = counter['f'] + counter['F']
-        g = counter['g'] + counter['G']
-        h = counter['h'] + counter['H']
-        i = counter['i'] + counter['I']
-        j = counter['j'] + counter['J']
-        k = counter['k'] + counter['K']
-        l = counter['l'] + counter['L']
-        m = counter['m'] + counter['M']
-        n = counter['n'] + counter['N']
-        o = counter['o'] + counter['O']
-        p = counter['p'] + counter['P']
-        q = counter['q'] + counter['Q']
-        r = counter['r'] + counter['R']
-        s = counter['s'] + counter['S']
-        t = counter['t'] + counter['T']
-        u = counter['u'] + counter['U']
-        v = counter['v'] + counter['V']
-        w = counter['w'] + counter['W']
-        x = counter['x'] + counter['X']
-        y = counter['y'] + counter['Y']
-        z = counter['z'] + counter['Z']
+        vowels = 0
+        vowels_list = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y']
+        for char in data:
+            if char in vowels_list:
+                vowels = vowels + 1
+
+        consonants = 0
+        consonants_list = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J', 'j', 'K', 'k', 'L', 'l',
+                           'M', 'm', 'N', 'n', 'p', 'P', 'R', 'r', 'S', 's', 'T', 't', 'W', 'w', 'X', 'x', 'Z', 'z']
+        for char in data:
+            if char in consonants_list:
+                consonants = consonants + 1
 
         if filename:
             with open(filename, "w") as file:
